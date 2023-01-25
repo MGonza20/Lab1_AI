e91f9d9c80b6f42c666db56d97467a552c8b6f12
MGonza20 <sara.paguaga@gmail.com>
2023-01-25 12:28:59 -0600

    refactoring code

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from PIL import Image
-import numpy as np
 import math 
+
+red = (255, 0, 0)
+green = (0, 255, 0)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 
 def checkPixel(pixel):
@@ -9,17 +13,26 @@ def checkPixel(pixel):
     b = pixel[2]
 
     # Euclidian Distance in RGB Color
-    if (pixel != (255, 0, 0) and pixel != (0, 255, 0) and pixel != (0, 0, 0) and pixel != (255, 255, 255)):
-        dRed =  math.pow((r - 255), 2) + math.pow((g - 0), 2) + math.pow((b - 0), 2)
-        dGreen = math.pow((r - 0), 2) + math.pow((g - 255), 2) + math.pow((b - 0), 2)
-        dBlack = math.pow((r - 0), 2) + math.pow((g - 0), 2) + math.pow((b - 0), 2)
-        dWhite = math.pow((r - 255), 2) + math.pow((g - 255), 2) + math.pow((b - 255), 2)
+    if (r, g, b) not in [red, green, black, white]:
+        rMinus255, rMinus0 = math.pow((r - 255), 2), math.pow((r - 0), 2) 
+        gMinus255, gMinus0 = math.pow((g - 255), 2), math.pow((g - 0), 2) 
+        bMinus255, bMinus0 = math.pow((b - 255), 2), math.pow((b - 0), 2) 
+
+        dRed = rMinus255 + gMinus0 + bMinus0
+        dGreen = rMinus0 + gMinus255 + bMinus0
+        dBlack = rMinus0 + gMinus0 + bMinus0
+        dWhite = rMinus255 + gMinus255 + bMinus255
+
         minVal = min(dRed, dGreen, dBlack, dWhite)
 
-        if minVal == dRed: return (255, 0, 0)
-        elif minVal == dGreen: return (0, 255, 0)
-        elif minVal == dBlack: return (0, 0, 0)
-        elif minVal == dWhite: return (255, 255, 255)
+        if minVal == dRed: 
+            return red
+        elif minVal == dGreen: 
+            return green
+        elif minVal == dBlack: 
+            return black
+        elif minVal == dWhite: 
+            return white
 
     else:
         return pixel
