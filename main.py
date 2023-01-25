1e7188754de54c2fc87bacfb672b5fc1ff486c55
MGonza20 <sara.paguaga@gmail.com>
2023-01-25 12:49:28 -0600

    Refactoring into OOP

--- main.py
+++ main.py
@@ -6,46 +6,53 @@ green = (0, 255, 0)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+class imgProcessing:
+    def __init__(self, img_path):
+        self.image = Image.open(img_path)
+        self.width, self.height = self.image.size
+        self.new_size = math.floor(self.width/4)
 
-def checkPixel(pixel):
-    r = pixel[0]
-    g = pixel[1]
-    b = pixel[2]
+    def checkPixel(self, pixel):
+        r = pixel[0]
+        g = pixel[1]
+        b = pixel[2]
 
-    # Euclidian Distance in RGB Color
-    if (r, g, b) not in [red, green, black, white]:
-        rMinus255, rMinus0 = math.pow((r - 255), 2), math.pow((r - 0), 2) 
-        gMinus255, gMinus0 = math.pow((g - 255), 2), math.pow((g - 0), 2) 
-        bMinus255, bMinus0 = math.pow((b - 255), 2), math.pow((b - 0), 2) 
+        # Euclidian Distance in RGB Color
+        if (r, g, b) not in [red, green, black, white]:
+            rMinus255, rMinus0 = math.pow((r - 255), 2), math.pow((r - 0), 2) 
+            gMinus255, gMinus0 = math.pow((g - 255), 2), math.pow((g - 0), 2) 
+            bMinus255, bMinus0 = math.pow((b - 255), 2), math.pow((b - 0), 2) 
 
-        dRed = rMinus255 + gMinus0 + bMinus0
-        dGreen = rMinus0 + gMinus255 + bMinus0
-        dBlack = rMinus0 + gMinus0 + bMinus0
-        dWhite = rMinus255 + gMinus255 + bMinus255
+            dRed = rMinus255 + gMinus0 + bMinus0
+            dGreen = rMinus0 + gMinus255 + bMinus0
+            dBlack = rMinus0 + gMinus0 + bMinus0
+            dWhite = rMinus255 + gMinus255 + bMinus255
 
-        minVal = min(dRed, dGreen, dBlack, dWhite)
+            minVal = min(dRed, dGreen, dBlack, dWhite)
 
-        if minVal == dRed: 
-            return red
-        elif minVal == dGreen: 
-            return green
-        elif minVal == dBlack: 
-            return black
-        elif minVal == dWhite: 
-            return white
+            if minVal == dRed: 
+                return red
+            elif minVal == dGreen: 
+                return green
+            elif minVal == dBlack: 
+                return black
+            elif minVal == dWhite: 
+                return white
 
-    else:
-        return pixel
+        else:
+            return pixel
+
+    def discretizeImg(self):
+        return [[ self.checkPixel(self.image.getpixel(((i/self.new_size) * self.width, (j/self.new_size) * self.height))) 
+                  for j in range(self.new_size)] for i in range(self.new_size)]
+
+    def saveImg(self):
+        grid = self.discretizeImg()
+        img = Image.new(mode="RGB", size=(self.new_size, self.new_size))
+        [img.putpixel((i, j), grid[i][j]) for i in range(self.new_size) for j in range(self.new_size)]
+        img.save("discretized_img.png")
+
+img_process = imgProcessing("turing.png")
+img_process.saveImg()
 
 
-lab = Image.open("turing.png")
-width = lab.size[0]
-height = lab.size[1]
-new_size = math.floor(width/4)
-
-grid = [[checkPixel(lab.getpixel(((i/new_size) * width, (j/new_size) * height))) 
-          for j in range(new_size)] for i in range(new_size)]
-
-img = Image.new(mode="RGB", size=(new_size, new_size))
-[img.putpixel((i, j), grid[i][j]) for i in range(new_size) for j in range(new_size)]
-img.save("discretized_img.png")
